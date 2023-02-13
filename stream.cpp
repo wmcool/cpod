e668d2bac81e72a12c4b0f6070347a828c77a06f
wmcool <973273711@qq.com>
2023-02-13 21:32:38 +0800

    fuck

--- stream.cpp
+++ stream.cpp
@@ -13,22 +13,26 @@ std::string pipe_line;
 vector<Point> get_incoming_data(int current_time, int pipe_fd, int length, string delimiter) {
     vector<Point> datas;
     string line;
-    while(true) {
+    while (true) {
         size_t pos = 0;
         std::string token;
         char buffer[71];
         vector<double> vals;
         int count;
         int valread = read(pipe_fd, buffer, 71);
-        if(valread > 0) {
-            for(int i=0;i<valread;i++) {
-                if(buffer[i] == '\n') {
-                        while ((pos = pipe_line.find(delimiter)) != std::string::npos) {
-                            token = pipe_line.substr(0, pos);
-                            vals.push_back(atof(token.c_str()));
-                            pipe_line.erase(0, pos + delimiter.length());
-                        }
-                    cout << "recv:" << pipe_line << endl;
+        if (valread > 0) {
+            for (int i = 0; i < valread; i++) {
+                if (buffer[i] == '\n') {
+                    while ((pos = pipe_line.find(delimiter)) != std::string::npos) {
+                        token = pipe_line.substr(0, pos);
+                        vals.push_back(atof(token.c_str()));
+                        pipe_line.erase(0, pos + delimiter.length());
+                    }
+                    cout << "recv: ";
+                    for (int i = 0; i < vals.size(); i++) {
+                        cout << vals[i] << ",";
+                        if (i == vals.size() - 1) cout << endl;
+                    }
 //                    if(!getline(file, line)) break;
 //                    while ((pos = line.find(delimiter)) != std::string::npos) {
 //                        token = line.substr(0, pos);
@@ -47,7 +51,7 @@ vector<Point> get_incoming_data(int current_time, int pipe_fd, int length, strin
                 }
             }
         }
-        if(count == length) break;
+        if (count == length) break;
     }
     return datas;
 }
